3f434d343e1aa9222ba85d6e623e61158b6a1cbd
Nlim97 <nobellimbu1@gmail.com>
2024-10-17 03:37:51 +0100

    hide api key

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,6 +1,7 @@
 
+
 export async function fetchWeatherData(cityName){
-    const apiKey = "XQUJ4S2ER8546CLR2EJYUXW8Y"
+    const apiKey = process.env.WEATHER_API_KEY
 
     const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName},UK?key=${apiKey}`)
     const parsedWeatherData = await weatherData.json()
